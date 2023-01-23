d1404b66d2715605898448c92ffa299d8c5d202e
ana-lapas <anapaulalapas@gmail.com>
2023-01-22 23:31:55 -0400

    alter port

--- src/index.js
+++ src/index.js
@@ -11,4 +11,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(transactionRouter);
 
-app.listen("5000", () => console.log("Server running on port 5000"))
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
